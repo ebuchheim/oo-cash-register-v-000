5cfe02001a3e323ecc7e02e5a82ea9c63c0c0e5e
ebuchheim <emma.buchheim@gmail.com>
2019-01-22 11:51:21 -0800

    add cash register logic

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
 
+
+class CashRegister
+
+    attr_accessor :total, :discount, :last_transcation
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items_array = []
+        @last_transcation = 0
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity = 1)
+        quantity.times do 
+            @items_array << title
+        end
+        @last_transcation = (price * quantity)
+        @total += (price * quantity)
+    end
+
+    def apply_discount
+        if !(@discount.to_f == 0.0)
+            discount_fraction = (100.0 - @discount)/100.0
+            @total = @total * discount_fraction
+            "After the discount, the total comes to $#{@total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @items_array
+    end
+
+    def void_last_transaction
+        @total = @total - @last_transcation
+    end
+
+end
